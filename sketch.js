b8a1ddec34270553f0959082a36ef402defd53fe
returnexitsuccess <returnexitsuccess@gmail.com>
2017-01-21 00:05:42 -0800

    added dummy features

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
-function setup() {
+var canvas;
+var x;
+var y;
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  x = mouseX;
+  y = mouseY;
+}
+
+function windowResized() {
+  canvas = resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  
+  background(51);
+  x = lerp(x, mouseX, 0.2);
+  y = lerp(y, mouseY, 0.2);
+  ellipse(x, y, 50);
+  ellipse(width - x, y, 50);
+  ellipse(x, height - y, 50);
+  ellipse(width - x, height - y, 50);
 }
