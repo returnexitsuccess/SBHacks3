e8358cd32643529ccca2d643fe1bc9552c19d44c
jkompella33 <jkompella24@gmail.com>
2017-01-21 17:36:56 -0800

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -1,6 +1,6 @@
 function getUserTweets(user) {
   //Gets an array of the tweet objects (not just tweet plaintext, json objects) of <user> and adds them to the global dictionary userTweets under the key, <user>
-  //e.g. user = "@BarackObama"
+  //e.g. user = "BarackObama"
 
   //dummy code
   userTweets[user] = [{
