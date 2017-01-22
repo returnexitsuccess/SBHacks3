d0fb132eba0cdc62b4734d0533718b0ea8757055
Joseph <jkompella@umail.ucsb.edu>
2017-01-21 17:21:40 -0800

    Added iteration method

--- analysis.js
+++ analysis.js
@@ -1,16 +1,37 @@
 function analyze(text) {
-	text = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
 	console.log(text);
+	text = text.replace(/(?:https?|ftp):\/\/[\n\S]+/g, '');
+	//console.log(text);
 	var new_text = text.split(/[^\w#']+/);
-	console.log(new_text);
+	//console.log(new_text);
 	var total_rating = 0;
-	for(var index in new_text) {
-		var word = new_text[index].toLowerCase();
-		if(afinn.hasOwnProperty(word)) {
-			var rating = afinn[word];
-			total_rating += rating;
+	for(var i = new_text.length-1; i >= 0; i--) {
+		var word = new_text[i].toLowerCase();
+		if(/^[a-zA-Z]+$/.test(word)) {
+			if(afinn.hasOwnProperty(word)) {
+				var rating = afinn[word];
+				//console.log(word, rating);
+				total_rating += rating;
+			}
+		}
+		else
+		{
+			new_text.splice(i, 1);
 		}
 	}
+	//console.log(new_text);
 	var avg_rating = total_rating/(new_text.length);
 	return avg_rating;
+}
+
+function iterator(list) {
+	for(var i = 0; i < 100; i++) {
+		if(list[i] == null)
+			break;
+		else {
+			var tweet = list[i];
+			var sentiment = analyze(tweet.text);
+			console.log(sentiment);
+		}
+	}
 }
